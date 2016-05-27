1f2c645500fe8a6d8933732c94bbad470bf344e2
unknown <usmithsx@LNSSIL-HR2SFV1.Lnssi.com>
2016-05-27 11:08:15 -0400

    commentform

---
+++ reactpractice/react-15.1.0/src/helloworld.js
@@ -0,0 +1,4 @@
+ReactDOM.render(
+	<hl>Hello, Worldsss!</hl>,
+	document.getElementById('example')
+);
---
+++ reactpractice/react-15.1.0/tut.js
@@ -0,0 +1,5 @@
+<script type="text/jsx">
+ReactDom.render(<h1>Hello, world!</h1>,
+document.getElementById('myDiv')
+);
+</script>
